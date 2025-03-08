6da43ab0e550bed7d7b8759f94a996ac5fd621ce
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2025-03-08 12:17:37 +0900

    루트 디렉토리를 인자로 전달하도록 수정

--- GenerateProject.bat
+++ GenerateProject.bat
@@ -3,6 +3,6 @@
 SET PROJECT_NAME=%1
 SET PYTHON_EXE=python.exe
 SET PYTHON_SCRIPT_PATH=%~dp0\Script\GenerateProject.py
-SET PYTHON_ARGS=--project_name=%PROJECT_NAME%
+SET PYTHON_ARGS=--project_name=%PROJECT_NAME% --root_directory=%~dp0
 
 %PYTHON_EXE% %PYTHON_SCRIPT_PATH% %PYTHON_ARGS%
