db4ef3580b132410d6b64d8257fcc209e733784f
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2025-03-08 12:07:47 +0900

    생성할 Visual Studio 프로젝트의 이름값 초기화

--- Script/GenerateProject.py
+++ Script/GenerateProject.py
@@ -1,1 +1,10 @@
-print("Hello World!")
+import sys
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--project_name", help="Name of the Visual Studio C# console project to create.")
+
+    args = parser.parse_args()
+
+    vs_project_name = args.project_name
