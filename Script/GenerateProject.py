7f2b15b7c0a7986475348789e97ad7bb685df821
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2025-03-08 12:38:28 +0900

    이미 프로젝트를 생성했다면 스크립트 종료

--- Script/GenerateProject.py
+++ Script/GenerateProject.py
@@ -13,7 +13,7 @@ def is_already_exist_vs_project(root_directory, project_name):
         return False
     
     return True
-    
+
 if __name__ == "__main__":    
     parser = argparse.ArgumentParser()
     parser.add_argument("--project_name", help="Name of the Visual Studio C# console project to create.")
@@ -29,8 +29,10 @@ if __name__ == "__main__":
         sys.exit(1)
 
     # 인자로 전달받은 프로젝트가 이미 존재한다면.
-
-
-    print(f"{project_name} 프로젝트 생성 시작.\n")
+    if is_already_exist_vs_project(root_directory, project_name):
+        print(f"\nERROR! '{project_name}' C# Visual Studio project is already exist.\n")
+        sys.exit(1)
+    
+    print(f"'{project_name}' 프로젝트 생성 시작.\n")
     command = f"dotnet new console --language \"C#\" --name {project_name}"
     os.system(command)
