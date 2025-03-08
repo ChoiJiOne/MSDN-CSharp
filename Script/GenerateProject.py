6f33d4a733eda9c00ec5d60637e70c8e5b2d1a4c
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2025-03-08 12:18:13 +0900

    파이썬 내에서 루트 디렉토리 설정

--- Script/GenerateProject.py
+++ Script/GenerateProject.py
@@ -1,10 +1,11 @@
 import sys
+import os
 import argparse
 
-if __name__ == "__main__":
+if __name__ == "__main__":    
     parser = argparse.ArgumentParser()
     parser.add_argument("--project_name", help="Name of the Visual Studio C# console project to create.")
-
+    parser.add_argument("--root_directory", help="MSDN-CSharp directory(Do not touch this directory).")
     args = parser.parse_args()
 
-    vs_project_name = args.project_name
+    root_directory = args.root_directory
