97843d4fb6c5f9dae70addb4a55c803f5a00402b
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2025-03-08 12:35:52 +0900

    Visual Studio 프로젝트가 이미 존재하는지 확인하는 기능 추가

--- Script/GenerateProject.py
+++ Script/GenerateProject.py
@@ -2,6 +2,18 @@ import sys
 import os
 import argparse
 
+def is_already_exist_vs_project(root_directory, project_name):
+    target_project_path = f"{root_directory}{project_name}"
+    target_project_file = f"{target_project_path}/{project_name}.csproj"
+
+    if not os.path.isdir(target_project_path):
+        return False
+    
+    if not os.path.isfile(target_project_file):
+        return False
+    
+    return True
+    
 if __name__ == "__main__":    
     parser = argparse.ArgumentParser()
     parser.add_argument("--project_name", help="Name of the Visual Studio C# console project to create.")
@@ -15,6 +27,9 @@ if __name__ == "__main__":
     if not project_name:
         print("\nERROR! Project name not provided.\nUSAGE: GenerateProject.bat <PROJECT_NAME>\n")
         sys.exit(1)
+
+    # 인자로 전달받은 프로젝트가 이미 존재한다면.
+
 
     print(f"{project_name} 프로젝트 생성 시작.\n")
     command = f"dotnet new console --language \"C#\" --name {project_name}"
