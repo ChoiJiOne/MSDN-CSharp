6929561fc664f0b11ca9a09ae6b0575f061c00be
CHOIJIONE\choij <choijione02@gmail.com>
2025-03-09 19:43:32 +0900

    프로젝트 생성 스크립트의 위치 변경 사항 적용

--- CSharpProgramming/GenerateProject.bat
+++ CSharpProgramming/GenerateProject.bat
@@ -2,7 +2,7 @@
 
 SET PROJECT_NAME=%1
 SET PYTHON_EXE=python.exe
-SET PYTHON_SCRIPT_PATH=%~dp0\Script\GenerateProject.py
+SET PYTHON_SCRIPT_PATH=%~dp0..\Script\GenerateProject.py
 SET PYTHON_ARGS=--project_name=%PROJECT_NAME% --root_directory=%~dp0
 
 %PYTHON_EXE% %PYTHON_SCRIPT_PATH% %PYTHON_ARGS%
