23d457eacbb17ed090851e2f79c8800d7eefef14
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2025-03-09 14:36:49 +0900

    MainReturnValTest 프로젝트 추가

---
+++ CommandLineArgs/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        StringBuilder builder = new();
+        builder.AppendLine("The following arguments are passed: ");
+
+        foreach (var arg in args)
+        {
+            builder.AppendLine($"Argument={arg}");
+        }
+
+        Console.WriteLine(builder.ToString());
+    }
+}
